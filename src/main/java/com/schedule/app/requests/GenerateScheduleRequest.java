b4ef89c2493adbb70f36bd226e0dc5c2ebba2a6b
thanhlocpc <nguyenthanhlocpc@gmail.com>
2023-03-01 13:17:34 +0700

    ++up

---
+++ src/main/java/com/schedule/app/requests/GenerateScheduleRequest.java
@@ -0,0 +1,14 @@
+package com.schedule.app.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenerateScheduleRequest {
+   private List<Integer> properties;
+}
